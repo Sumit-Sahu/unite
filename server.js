94dc7d45be9d2fffe164c117f714699174e461e9
Sumit Sahu <sumiiitjsahu@gmail.com>
2021-01-18 10:59:06 +0530

    display video grid

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').Server(app);
+const { v4 } = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
-    res.status(200).send("zoom clone");
+    res.redirect(`/${v4()}`);
 })
 
-
-
-
-
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+})
 
 
 server.listen(3000);
