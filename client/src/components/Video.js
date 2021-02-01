7d854711da066e5a9ae24e43354deb211614d699
Sumit Sahu <sumiiitjsahu@gmail.com>
2021-02-01 15:58:29 +0530

    remove user video on diconnect

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -6,9 +6,15 @@ let peers = {};
 
 const Video = () => {
     const localVideoref = useRef();
-    const {url} = useParams();
+    const { url } = useParams();
 
     useEffect(() => {
+
+        window.onpopstate = event => {
+            // To do
+            // leave user from socket room
+            console.log(event);
+        }
 
         socket.on('error', error => {
             console.log(error);
@@ -25,21 +31,31 @@ const Video = () => {
             audio: true
         }).then(stream => {
             localVideoref.current.srcObject = stream;
+            // const videoGrid = document.getElementById("video-grid");
             localVideoref.current.addEventListener('loadedmetadata', () => {
                 localVideoref.current.play();
             })
             peer.on('call', call => {
                 call.answer(stream);
+                peers[call.peer] = call;
                 const video = document.createElement('video');
                 call.on('stream', userVideoStream => {
+                    userVideoStream.oninactive = () => {
+                        console.log('stream get inactive')
+                    }
+                    console.log(userVideoStream)
                     addVideoStream(video, userVideoStream);
                 })
                 call.on('close', () => {
                     console.log('disconnected');
+                    video.remove();
                 })
                 call.on('error', (error) => {
                     console.log(error);
                 })
+                call.oniceconnectionstatechange = () => {
+                    console.log("ice conne state change")
+                }
             })
 
             socket.on('user-connected', (userId) => {
@@ -49,8 +65,13 @@ const Video = () => {
         });
 
         socket.on('user-disconnected', userId => {
-            console.log("user disconnected",userId);
-            if(peers[userId]) peers[userId].close();
+            console.log("user disconnected", userId);
+            if (peers[userId]) {
+                peers[userId].close();
+                delete peers[userId];
+            }
+            let videoGrid = document.getElementById('video-grid');
+            changeVideoCss(videoGrid);
         });
 
         peer.on('open', id => {
@@ -66,10 +87,17 @@ const Video = () => {
             const call = peer.call(userId, stream);
             const video = document.createElement('video');
             call.on('stream', userVideoStream => {
+                userVideoStream.oninactive = () => {
+                    console.log('stream get inactive 2')
+                }
                 addVideoStream(video, userVideoStream);
             });
-        
-            call.on('close', ()=> {
+
+            call.oniceconnectionstatechange = () => {
+                console.log("ice conne state change")
+            }
+
+            call.on('close', () => {
                 video.remove();
                 console.log('closed call');
             });
@@ -77,7 +105,7 @@ const Video = () => {
             call.on('error', (error) => {
                 console.log(error);
             })
-        
+
             peers[userId] = call;
         }
 
@@ -88,6 +116,43 @@ const Video = () => {
                 video.play();
             })
             videoGrid.append(video);
+            changeVideoCss(videoGrid);
+        }
+
+        let changeVideoCss = main => {
+            let elms = document.getElementById("video-grid").childElementCount;
+            let widthMain = main.offsetWidth
+            let minWidth = "30%"
+            if ((widthMain * 30 / 100) < 300) {
+                minWidth = "300px"
+            }
+            let minHeight = "40%"
+
+            let height = String(100 / elms) + "%"
+            let width = ""
+
+            if (elms === 0 || elms === 1) {
+                width = "100%"
+                height = "100%"
+            } else if (elms === 2) {
+                width = "45%"
+                height = "100%"
+            } else if (elms === 3 || elms === 4) {
+                width = "35%"
+                height = "50%"
+            } else {
+                width = String(100 / elms) + "%"
+            }
+
+            let videos = main.querySelectorAll("video")
+            for (let a = 0; a < videos.length; ++a) {
+                videos[a].style.minWidth = minWidth
+                videos[a].style.minHeight = minHeight
+                videos[a].style.setProperty("width", width)
+                videos[a].style.setProperty("height", height)
+            }
+
+            return { minWidth, minHeight, width, height }
         }
 
     }, [url])
@@ -95,9 +160,15 @@ const Video = () => {
 
 
     return (
-        <div id="video-grid">
-            <video ref={localVideoref} id="my-video"></video>
-        </div>
+        <>
+            <div className="video-container">
+                <div className="video-main">
+                    <div id="video-grid" className="d-flex flex-row justify-content-center flex-wrap">
+                        <video ref={localVideoref} id="my-video" style={{border: "5px solid #fae8eb", margin: "10px", objectFit: "fill", width: "100%", height:"100%"}}></video>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
